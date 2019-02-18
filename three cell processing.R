6a432d4398aeb1bff120c0ff814392ab1a314d20
William Thomas <wthomas@treecreeper.stats.ox.ac.uk>
2019-02-18 22:59:04 +0000

    Now on non-log scale.

--- three cell processing.R
+++ three cell processing.R
@@ -9,9 +9,16 @@ library(affy)
 # Import .CEL files as an AffyBatch; need .CEL files in current working directory
 three_cell <- ReadAffy()
 
-three_cell <- rma(three_cell)
+# Examine raw values
+expr_three_cell <- exprs(three_cell)
 
-gse19830 <- exprs(three_cell)
+# Perform rma
+rma_three_cell <- affy::rma(three_cell)
+
+gse19830 <- exprs(rma_three_cell)
+head(gse19830)
+
+gse19830 <- 2^gse19830
 head(gse19830)
 
 library(R.matlab)
